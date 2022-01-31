dc96cdcb0446ff496c5c694f26612ba0efcb922e
Mara Dusevic <s200494@students.aie.edu.au>
2022-01-31 12:14:27 +1100

    Updated visuals of graph
    Added style sheet and zoom function

--- DialogueSystem/Assets/Scripts/Dialogue/Editor/DialogueGraphView.cs
+++ DialogueSystem/Assets/Scripts/Dialogue/Editor/DialogueGraphView.cs
@@ -11,9 +11,16 @@ public class DialogueGraphView : GraphView
      
      public DialogueGraphView()
      {
+          styleSheets.Add(Resources.Load<StyleSheet>("DialogueGraph"));
+          SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+          
           this.AddManipulator(new ContentDragger());
           this.AddManipulator(new SelectionDragger());
           this.AddManipulator(new RectangleSelector());
+
+          GridBackground grid = new GridBackground();
+          Insert(0, grid);
+          grid.StretchToParentSize();
 
           AddElement(GenerateEntryPointNode());
      }
